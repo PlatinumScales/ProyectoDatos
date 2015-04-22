322a5898ead97ec52224a6749245039a3fa42320
Alejandro <Alejandromc14@gmail.com>
2015-04-22 11:05:38 -0600

    added methods

--- MusicPlayer/MusicPlayer/listaDeListas.h
+++ MusicPlayer/MusicPlayer/listaDeListas.h
@@ -1,14 +1,37 @@
 #pragma once
 #include "cancionLDC.h"
+#include "cancion.h"
 
 class listaDeListas
 {
+
+private:
+	cancionLDC * cab;
+	int size;
+
 public:
 	listaDeListas();
 	~listaDeListas();
 
-private:
-	cancionLDC * cab;
+	void insertar(cancionLDC sl); //Inserta una nuvea lista
+	void insertar(int nlista, cancion song); //inserta la cancion "song" en la lista numero "nlista"
+
+	void eliminar(cancionLDC sl);
+	void eliminar(int nlista);
+	void eliminar(int nlista, cancionLDC sl);
+	void eliminar(int nlista, cancionLDC sl);
+
+	int largo(); 
+	cancionLDC contiene(char* _nombre); //devuleve una sola lista con todas las canciones que contengan _nombre en todas las listas
+
+
+
+
+
+
+
+
+
 
 };
 
